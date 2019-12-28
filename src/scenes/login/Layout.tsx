3bd7e43200889f158b52a667fd3b41e86ed71da7
Marcoo09 <marcofiorito1@hotmail.com>
2019-12-28 01:27:23 -0300

    structure of layout

--- src/scenes/login/Layout.tsx
+++ src/scenes/login/Layout.tsx
@@ -2,9 +2,49 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {styles} from './styles';
 import {LayoutProps} from './types';
+import {TextInput} from 'react-native-paper';
+import {Button} from 'react-native-paper';
 
 export const Layout: React.FunctionComponent<LayoutProps> = () => (
   <View style={styles.container}>
-    <Text>Hola</Text>
+    <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>LOGO</Text>
+    </View>
+    <View style={{flex: 1}}>
+      <Text>Welcome Back</Text>
+      <Text>
+        Lorem ipsum text dummy sha sabes Lorem ipsum text dummy sha sabes Lorem
+        ipsum text dummy sha sabes Lorem ipsum text dummy sha sabes Lorem ipsum
+        text dummy sha sabes
+      </Text>
+    </View>
+    <View style={[styles.secondaryContainer, {flex: 2}]}>
+      <TextInput
+        label={'Email'}
+        maxLength={40}
+        mode={'flat'}
+        placeholder={'Email'}
+        onChangeText={() => console.warn('cambio el texto')}
+      />
+      <TextInput
+        label={'Password'}
+        maxLength={40}
+        mode={'flat'}
+        placeholder={'Password'}
+        onChangeText={() => console.warn('cambio el texto')}
+        secureTextEntry={true}
+      />
+    </View>
+    <View style={{flex: 1, alignItems: 'flex-end'}}>
+      <Text>Forgot Password?</Text>
+    </View>
+    <View style={styles.secondaryContainer}>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Login
+      </Button>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Sign up
+      </Button>
+    </View>
   </View>
 );
--- src/scenes/login/styles.ts
+++ src/scenes/login/styles.ts
@@ -1,20 +1,20 @@
 import {StyleSheet} from 'react-native';
 import {Palette} from '../../styles/Color';
+import {heightPercentageToDP} from 'react-native-responsive-screen';
 
 export const styles = StyleSheet.create({
-  cellBackgroungColor: {
-    backgroundColor: Palette.White,
-  },
-  challengePlayer: {
-    borderTopColor: Palette.Gray2Theme,
-    borderTopWidth: 0.5,
-    height: 111,
-  },
   container: {
     flex: 1,
+    paddingHorizontal: 30,
   },
   noPadding: {
     backgroundColor: Palette.White,
     padding: 0,
+  },
+  secondaryContainer: {
+    flex: 1,
+    paddingBottom: heightPercentageToDP(10),
+    paddingTop: heightPercentageToDP(10),
+    justifyContent: 'space-between',
   },
 });
