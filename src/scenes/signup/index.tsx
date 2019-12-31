7771deac8755d1fa40970b762199e2188be69b6f
Marcoo09 <marcofiorito1@hotmail.com>
2019-12-31 07:15:29 -0300

    linking library and redux mapping

--- src/scenes/signup/index.tsx
+++ src/scenes/signup/index.tsx
@@ -6,10 +6,12 @@ import {Layout} from './Layout';
 import {useNavigatesTo} from '../../customHooks/useNavigatesTo';
 import {Route} from '../../navigation/Route';
 import {NavigationScreenComponent, NavigationParams} from 'react-navigation';
+import {useRedux} from './redux/useRedux';
 
 export const SignUp: NavigationScreenComponent<NavigationParams, {}> = ({
   navigation,
 }) => {
+  // const {createNewAccount} = useRedux();
   const navigateToLogin = useNavigatesTo({navigation, route: Route.Login});
   return (
     <SceneContainer forceInset={outerInset} style={styles.noPadding}>
