71eff5f4666c4f8f3ebcda1d94b8a81ca0dc8771
Marcoo09 <marcofiorito1@hotmail.com>
2020-02-17 21:47:23 -0300

    add swagger to the backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('YoDono')
+    .setDescription('The YoDono API description')
+    .setVersion('0.1')
+    .addTag('YoDono')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
